34d76ee1f6421122eae4444855939aeafb0c5ef8
DeepikaRaji-EEE <deepikaeee2000@gmail.com>
2026-02-09 12:31:19 +0530

    Create if else task.py

---
+++ if else task.py
@@ -0,0 +1,47 @@
+'''#Hello world!
+print("Hello world!")'''
+
+'''n=int(input("Enter a number:"))
+m=int(input("Enter a number:"))
+if n<m:
+ print('n')'''
+
+'''n=int(input("Enter a number:"))
+m=int(input("Enter a number:"))
+if n>m:
+    print('m')'''
+
+'''a=int(input("Enter a number:"))
+b=a**2
+if b>50:
+    print("yes")
+else:
+    print("no")'''
+
+'''a=int(input("Enter a number:"))
+b=a**2
+if b<50:
+   print("yes")
+else:
+    print("no")'''
+
+
+'''mark=int(input("Enter a number:"))
+if mark>=50:
+     print("pass")
+else:
+    print("fail")'''
+
+'''
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+if a-b==0:
+    print("yes")
+else:
+    print("no")'''
+
+'''a=int(input("Enter a number:"))
+if a>=10 and a<=99:
+    print("yes")
+else:
+    print("no")'''
