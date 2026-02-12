c74fa9e1aff55527c2820c1ebbf52b970b38342f
DeepikaRaji-EEE <deepikaeee2000@gmail.com>
2026-02-12 12:05:02 +0530

    Create Deepika.py

---
+++ Deepika.py
@@ -0,0 +1,92 @@
+# 12345678910
+n=int(input("Enter a number:"))
+for i in range(1,n+1,1):
+    if n%1==0:
+     print(i)
+
+
+
+#NESTED IF
+ch=input("Enter a character:")
+if ch>='a' and ch<='z':
+    print('ch')
+else:
+ print("consonant")
+
+
+
+  #ASCII values:
+      0-48.....9-57
+      A-65.....Z-90
+      a-97.....z-122
+      space-32
+      
+n=int(input("enter a number:"))
+ch=chr(n)
+if n>=65 or n<=90:
+     print("yes")
+
+
+  
+a=int(input("enter a number:"))
+ 25->5
+ 16->4
+ 9->3
+ 121->11
+
+#formula=number**0.5
+c=10**0.5
+print(c)
+
+
+  #for loop
+
+#54321
+for i in range(5,0,-1):
+  print(i,"end=")
+print() 
+
+
+
+ #*****
+for j in range(1,4,1):
+ for i in range(1,4,1):
+   print(i*j,end=" ")
+print() 
+
+
+#2 4 6 8 10 12 14 16 18 20
+
+for i in range(1,n+1,1):
+     if i%2==0:
+         print(i)
+
+
+ #2 4 6 8 10 12 14 16 18 20
+
+n=int(input("enter the end value:"))
+for i in range(2,n+1,2):
+       print(i)
+
+       
+#while loop
+
+#1 2 3 4 5
+for i in range(1,6,1):
+    print(i,end=" ")
+    j=1
+while j<6:
+    print(j,end=" ")
+    j=j+1
+    
+
+#5 4 3 2 1
+for i in range(5,0,-1):
+  print(i,end=" ")
+  i=5
+  while i>0:
+      print(i,end=" ")
+      i=i-1
+
+
+
