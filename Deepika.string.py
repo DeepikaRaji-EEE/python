0e76b22830be94ee42ff5313f3cb5eb433041cca
DeepikaRaji-EEE <deepikaeee2000@gmail.com>
2026-02-12 13:30:12 +0530

    Create Deepika.string.py

---
+++ Deepika.string.py
@@ -0,0 +1,24 @@
+#a="python"
+#print(a[:len(a)])
+print("20".zfill(5))'''
+
+'''
+n="welcome to python programming"
+print(n.split())
+print(n.split('e'))
+m=n.split()
+print(''.join('m'))
+print(n.rsplit('o'))
+print(n.split('0'))   
+print(n.startswith("wel"))
+print(n.endswith("ing"))
+
+
+#string formating:
+
+Name="prathika"
+Course="python"
+Batch="11.30-1.30"
+Hours="30"
+print("The course","Handled by",Name,"At",Batch)'''
+
